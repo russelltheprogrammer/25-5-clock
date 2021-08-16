1b933008b806753f5989db4a7c19b5711b1ffff4
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-08-16 14:00:19 -0400

    Initial Setup

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Link to 25 + 5 Clock In Process --> [Javascript Calculator](http://russelltheprogrammer.github.io/25-5-clock/)
+
+Note: This project was done for the FCC curriculum for the Front End Libraries Certificate so sometimes there is extra code needed to pass the tests.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>25 + 5 Clock</h1>
       </header>
+      <footer className="footer">
+        Coded By <a 
+        target="_blank" rel="noreferrer" href="https://github.com/russelltheprogrammer">
+        RussellTheProgrammer</a> for FCC curriculum
+      </footer>
     </div>
   );
 }
