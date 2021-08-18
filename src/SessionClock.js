983a1f8a54006ec278954183628b09a495bff4dd
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-08-18 10:56:52 -0400

    Work on javascript

--- src/SessionClock.js
+++ src/SessionClock.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './index.scss';
-import convertMS from "./convertMS";
+import convertMMSS from "./convertMMSS";
 
 const SessionClock = ({ sessionCount }) => {
     
