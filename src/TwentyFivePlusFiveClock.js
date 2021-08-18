78dd6ea3d92b6eff79a92c7874d70d3b36e96525
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-08-18 16:14:24 -0400

    Work on functionality

--- src/TwentyFivePlusFiveClock.js
+++ src/TwentyFivePlusFiveClock.js
@@ -4,15 +4,27 @@ import SessionClock from './SessionClock';
 
 const TwentyFivePlusFiveClock = () => {
 
-const [sessionCount, setSessionCount] = useState(25);
-const [breakCount, setBreakCount] = useState(5);
+const [sessionCount, setSessionCount] = useState(1500000);
+const [breakCount, setBreakCount] = useState(300000);
+const [time, setTime] = useState(1500000)
 
+const startClock = () => {
+
+    setInterval(() => { 
+     setTime(time - 1000)
+    }, 1000);
+}
+
+const stopClock = () => {
+
+}
+ 
     return ( 
         <div>
             <div id="container-flex" className="d-flex flex-column justify-content-center align-items-center">
                 <div className="row">
                     <div className="col-12">
-                        <SessionClock sessionCount={sessionCount} />
+                        <SessionClock sessionCount={time/1000} />
                     </div>
                 </div>
                 <div className="row">
@@ -22,25 +34,25 @@ const [breakCount, setBreakCount] = useState(5);
                                 <div className="col-12"><label id="break-label">Break Length</label></div>
                             </div>
                             <div className="row">
-                                <div className="col-4"><button id="break-increment" onClick={() => breakCount >59 ? breakCount : setBreakCount(breakCount + 1)}>
-                                    <i class="fas fa-angle-double-up"></i></button></div>
-                                <div className="col-4 number-manipulation">{breakCount}</div>
-                                <div className="col-4"><button id="break-decrement" onClick={() => breakCount <=0 ? breakCount : setBreakCount(breakCount - 1)}>
-                                    <i class="fas fa-angle-double-down"></i></button></div>
+                                <div className="col-4"><button id="break-increment" onClick={() => breakCount >3540000 ? breakCount : setBreakCount(breakCount + 60000)}>
+                                    <i className="fas fa-angle-double-up"></i></button></div>
+                                <div className="col-4 number-manipulation">{breakCount/60000}</div>
+                                <div className="col-4"><button id="break-decrement" onClick={() => breakCount <=0 ? breakCount : setBreakCount(breakCount - 60000)}>
+                                    <i className="fas fa-angle-double-down"></i></button></div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-6">
+                    <div className="col-6">
                         <div className="container">
                             <div className="row">
                                 <div className="col-12"><label id="session-label">Session Length</label></div>
                             </div>
                             <div className="row">
-                                <div className="col-4"><button id="session-increment" onClick={() => sessionCount >59 ? sessionCount : setSessionCount(sessionCount + 1)}>
-                                    <i class="fas fa-angle-double-up"></i></button></div>
-                                <div className="col-4 number-manipulation">{sessionCount}</div>
-                                <div className="col-4"><button id="session-decrement" onClick={() => sessionCount <=0 ? sessionCount : setSessionCount(sessionCount - 1)}>
-                                    <i class="fas fa-angle-double-down"></i></button></div>
+                                <div className="col-4"><button id="session-increment" onClick={() => sessionCount >3540000 ? sessionCount : setSessionCount(sessionCount + 60000) + setTime(sessionCount + 60000)}>
+                                    <i className="fas fa-angle-double-up"></i></button></div>
+                                <div className="col-4 number-manipulation">{sessionCount/60000}</div>
+                                <div className="col-4"><button id="session-decrement" onClick={() => sessionCount <=0 ? sessionCount : setSessionCount(sessionCount - 60000) + + setTime(sessionCount - 60000)}>
+                                    <i className="fas fa-angle-double-down"></i></button></div>
                             </div>
                         </div>
                     </div>
@@ -48,8 +60,8 @@ const [breakCount, setBreakCount] = useState(5);
                 <div className="row">
                     <div className="col-12">     
                         <div id="ssr-buttons">
-                            <button id="start_stop"><i class="fas fa-play-circle"></i><i class="fas fa-pause-circle"></i></button>
-                            <button id="reset" onClick={() => setSessionCount(25) + setBreakCount(5)}><i class="fas fa-redo"></i></button>
+                            <button id="start_stop" onClick={() => startClock()}><i className="fas fa-play-circle"></i><i className="fas fa-pause-circle"></i></button>
+                            <button id="reset" onClick={() => setSessionCount(1500000) + setBreakCount(300000) + setTime(1500000)}><i className="fas fa-redo"></i></button>
                         </div>
                     </div>  
                 </div>    
--- src/convertMMSS.js
+++ src/convertMMSS.js
@@ -1,6 +1,6 @@
 function convertMMSS(value) {
 
-const sec = parseInt(value, 10) * 60; // convert value to number if it is a string
+const sec = parseInt(value, 10); // convert value to number if it is a string
 let minutes = Math.floor((sec / 60)); // get minutes
 let seconds = sec - (minutes * 60); // get seconds
 // add 0 if value < 10; Example 2 => 02
