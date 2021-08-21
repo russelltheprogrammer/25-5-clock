d599c70fc13b7d376721f8010270697e15fc644b
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-08-21 16:27:43 -0400

    Work on timer functionality

--- src/SessionClock.js
+++ src/SessionClock.js
@@ -2,13 +2,16 @@ import React from "react";
 import './index.scss';
 import convertMMSS from "./convertMMSS";
 
-const SessionClock = ({ time }) => {
+const SessionClock = ({ time, sessionStatus }) => {
     
 let MMSS = convertMMSS(time);
+let sessionTitle = "";
+
+sessionStatus ? sessionTitle="Session" : sessionTitle="Break";
 
     return ( 
         <div id="session-clock">
-            <label id="timer-label">Session</label>
+            <label id="timer-label">{sessionTitle}</label>
             <div id="time-left">{MMSS}</div>
         </div>
      );
--- src/TwentyFivePlusFiveClock.js
+++ src/TwentyFivePlusFiveClock.js
@@ -7,26 +7,66 @@ const TwentyFivePlusFiveClock = () => {
 
 const [sessionCount, setSessionCount] = useState(1500000);
 const [breakCount, setBreakCount] = useState(300000);
+const [sessionCountToggle, setSessionCountToggle] = useState(true);
+const [breakCountToggle, setBreakCountToggle] = useState(false);
 const [time, setTime] = useState(1500000);
 const [delay, setDelay] = useState(1000);
 const [isRunning, setIsRunning] = useState(false);
 
 useInterval(() => { 
-     setTime(time - 1000)
-    }, isRunning ? delay : null);
+    if(time < 1000){
+        handleSessionAndBreakToggle()
+    }
+    else if(time > 0 && sessionCountToggle) { 
+        setTime(time - 1000)
+    }
+    else if(time > 0 && breakCountToggle){
+        setTime(time - 1000)
+    }
+}, isRunning ? delay : null);
 
 const handlePlayStop = () => isRunning ? setIsRunning(false) : setIsRunning(true);
 
-const StopClock = () => {
+const handleSessionAndBreakToggle = () => sessionCountToggle 
+        ? setSessionCountToggle(false) + setBreakCountToggle(true) + setTime(breakCount)
+        : setSessionCountToggle(true) + setBreakCountToggle(false) + setTime(sessionCount);
 
-}
- 
+const handleIncrement = (typeOfCount) => {
+    if( typeOfCount >3540000 || isRunning){
+        return typeOfCount;
+    }
+    else {
+        if(typeOfCount === breakCount){
+            return setBreakCount(typeOfCount + 60000);
+        }
+        else if (typeOfCount === sessionCount){
+            return setSessionCount(typeOfCount + 60000) + setTime(typeOfCount + 60000);
+        }
+    }
+};
+
+const handleDecrement = (typeOfCount) => {
+    if( typeOfCount <=60000 || isRunning){
+        return typeOfCount;
+    }
+    else {
+        if(typeOfCount === breakCount){
+            return setBreakCount(typeOfCount - 60000);
+        }
+        else if (typeOfCount === sessionCount){
+            return setSessionCount(typeOfCount - 60000) + setTime(typeOfCount - 60000);
+        }
+    }
+};
+
+const handleReset = () => setSessionCount(1500000) + setBreakCount(300000) + setTime(1500000) + setIsRunning(false) + setSessionCountToggle(true);
+
     return ( 
         <div>
             <div id="container-flex" className="d-flex flex-column justify-content-center align-items-center">
                 <div className="row">
                     <div className="col-12">
-                        <SessionClock time={time/1000} />
+                        <SessionClock time={time/1000} sessionStatus={sessionCountToggle} />
                     </div>
                 </div>
                 <div className="row">
@@ -36,10 +76,10 @@ const StopClock = () => {
                                 <div className="col-12"><label id="break-label">Break Length</label></div>
                             </div>
                             <div className="row">
-                                <div className="col-4"><button id="break-increment" onClick={() => breakCount >3540000 ? breakCount : setBreakCount(breakCount + 60000)}>
+                                <div className="col-4"><button id="break-increment" onClick={() => handleIncrement(breakCount)}>
                                     <i className="fas fa-angle-double-up"></i></button></div>
                                 <div className="col-4 number-manipulation">{breakCount/60000}</div>
-                                <div className="col-4"><button id="break-decrement" onClick={() => breakCount <=0 ? breakCount : setBreakCount(breakCount - 60000)}>
+                                <div className="col-4"><button id="break-decrement" onClick={() => handleDecrement(breakCount)}>
                                     <i className="fas fa-angle-double-down"></i></button></div>
                             </div>
                         </div>
@@ -50,11 +90,9 @@ const StopClock = () => {
                                 <div className="col-12"><label id="session-label">Session Length</label></div>
                             </div>
                             <div className="row">
-                                <div className="col-4"><button id="session-increment" onClick={() => sessionCount >3540000 ? sessionCount : setSessionCount(sessionCount + 60000) + setTime(sessionCount + 60000)}>
-                                    <i className="fas fa-angle-double-up"></i></button></div>
+                                <div className="col-4"><button id="session-increment" onClick={() => handleIncrement(sessionCount)}><i className="fas fa-angle-double-up"></i></button></div>
                                 <div className="col-4 number-manipulation">{sessionCount/60000}</div>
-                                <div className="col-4"><button id="session-decrement" onClick={() => sessionCount <=0 ? sessionCount : setSessionCount(sessionCount - 60000) + + setTime(sessionCount - 60000)}>
-                                    <i className="fas fa-angle-double-down"></i></button></div>
+                                <div className="col-4"><button id="session-decrement" onClick={() => handleDecrement(sessionCount)}><i className="fas fa-angle-double-down"></i></button></div>
                             </div>
                         </div>
                     </div>
@@ -63,7 +101,7 @@ const StopClock = () => {
                     <div className="col-12">     
                         <div id="ssr-buttons">
                             <button id="start_stop" onClick={() => handlePlayStop()}><i className="fas fa-play-circle"></i><i className="fas fa-pause-circle"></i></button>
-                            <button id="reset" onClick={() => setSessionCount(1500000) + setBreakCount(300000) + setTime(1500000)}><i className="fas fa-redo"></i></button>
+                            <button id="reset" onClick={() => handleReset()}><i className="fas fa-redo"></i></button>
                         </div>
                     </div>  
                 </div>    
